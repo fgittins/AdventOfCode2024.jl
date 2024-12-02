03f6d93b12f7f984d1110f5733d9f4348fa6d2cc
Fabian Gittins <fabiangittins@gmail.com>
2024-12-02 20:39:52 +0100

    Separate tests for Parts 1 and 2

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,21 @@ using AdventOfCode2024
 using Test
 
 @testset "Day01" begin
-    @test AdventOfCode2024.Day01.day01() == (2580760, 25358365)
+    part1, part2 = AdventOfCode2024.Day01.day01()
+    @testset "part1" begin
+        @test part1 == 2580760
+    end
+    @testset "part2" begin
+       @test part2 == 25358365 
+    end
 end
 
 @testset "Day02" begin
-    @test AdventOfCode2024.Day02.day02() == (218, 290)
+    part1, part2 = AdventOfCode2024.Day02.day02()
+    @testset "part1" begin
+        @test part1 == 218
+    end
+    @testset "part2" begin
+        @test part2 == 290
+    end
 end
