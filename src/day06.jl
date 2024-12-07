8cadb73e40bb537986357ba498f287022c870690
Fabian Gittins <fabiangittins@gmail.com>
2024-12-07 23:40:54 +0100

    Clean-up Day 6 solution

--- src/day06.jl
+++ src/day06.jl
@@ -1,47 +1,28 @@
 module Day06
 
-function turn_right(direction)
-    if direction == CartesianIndex(-1, 0)
-        direction = CartesianIndex(0, 1)
-    elseif direction == CartesianIndex(0, 1)
-        direction = CartesianIndex(1, 0)
-    elseif direction == CartesianIndex(1, 0)
-        direction = CartesianIndex(0, -1)
-    elseif direction == CartesianIndex(0, -1)
-        direction = CartesianIndex(-1, 0)
-    end
-    direction
-end
-
-const directions = Dict('^' => CartesianIndex(-1, 0),
-                        '>' => CartesianIndex(0, 1),
-                        'v' => CartesianIndex(1, 0),
-                        '<' => CartesianIndex(0, -1))
+turn_right(direction) = CartesianIndex(direction[2], -direction[1])
 
 function part1(map)
-    position = start = findfirst(i -> i == '^', map)
-    direction = directions[map[start]]
-    positions = Set([start])
-    while true
-        position_new = position + direction
-        !checkbounds(Bool, map, position_new) && break
-        if map[position_new] == '#'
+    position = findfirst(i -> i == '^', map)
+    direction = CartesianIndex(-1, 0)
+    positions = Set{CartesianIndex{2}}()
+    while checkbounds(Bool, map, position + direction)
+        if map[position + direction] == '#'
             direction = turn_right(direction)
             continue
         end
-        position = position_new
+        position += direction
         push!(positions, position)
     end
-    length(positions), delete!(positions, start)
+    length(positions), positions
 end
 
 function is_in_loop(map, position_obstacle)
     position = findfirst(i -> i == '^', map)
-    direction = directions[map[position]]
+    direction = CartesianIndex(-1, 0)
     positions_directions = Set([(position, direction)])
-    while true
+    while checkbounds(Bool, map, position + direction)
         position_new = position + direction
-        !checkbounds(Bool, map, position_new) && return false
         if map[position_new] == '#' || position_new == position_obstacle
             direction = turn_right(direction)
             continue
@@ -51,23 +32,15 @@ function is_in_loop(map, position_obstacle)
         position_direction ∈ positions_directions && return true
         push!(positions_directions, position_direction)
     end
+    return false
 end
 
-function part2(map, positions)
-    ans = 0
-    for position_direction ∈ positions
-        if is_in_loop(map, position_direction)
-            ans += 1
-        end
-    end
-    ans
-end
+part2(map, positions) = sum(is_in_loop(map, position_obstacle) for position_obstacle ∈ positions)
 
 function day06()
     map = reduce(vcat, (permutedims(collect(s)) for s ∈ readlines(joinpath(@__DIR__, "..", "data", "day06.txt"))))
-    ans1, positions = part1(map)
-    ans2 = part2(map, positions)
-    (ans1, ans2)
+    ans, positions = part1(map)
+    (ans, part2(map, positions))
 end
 
 end
